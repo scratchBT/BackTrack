567c77a37d9db000c5fedfa1cfa891adaf6614e3
Elena-Netepenko <elen.netepenko@gmail.com>
2024-01-24 20:08:11 -0800

    html skeleton for authentication component rendering

--- client/src/components/SignupComp.jsx
+++ client/src/components/SignupComp.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 const SignupComp = () => {
     return (
-      <div>
-        <h1>Sign up to start listening</h1>
-        <form>
-            <input type='text' placeholder='First name'></input>
-            <input type='email' placeholder='name@domain.com'></input>
-            <input type='password' placeholder='password'></input>
-            <button>Sign up</button>
-        </form>
-        <p>or</p>
-        <div>
-            <button>Sign up with Google</button>
-            <button>Sign up with Facebook</button>
+      <div className='authenticationPage--container'>
+        <div className='authenticationComp--container'>
+            <h1>Sign up to start listening</h1>
+            <form>
+                <label htmlFor='input--name'>Name</label>
+                <input type='text' placeholder='First name' id='input--name'></input>
+                <label htmlFor='input--email'>Email</label>
+                <input type='email' placeholder='name@domain.com' id='input--email'></input>
+                <label htmlFor='input--password'>Password</label>
+                <input type='password' placeholder='password' id='input--password'></input>
+                <button className='btn'>Sign up</button>
+            </form>
+            <p>or</p>
+            <div className='btn--container'>
+                <button className='btn'>Sign up with Google</button>
+                <button className='btn'>Sign up with Facebook</button>
+            </div>
+            <p>Already have an account? <a>Log in here</a>.</p>
         </div>
       </div>
     )
